611a0ab6161a8e8d27b9c938439a626e7fcc4901
Mahmoud-ahmed-JX <shahdelfares515@gmail.com>
2025-12-22 14:59:59 +0200

    patient , appointments and medical record Entities

---
+++ HospitalDAL/Entities/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDAL.Entities
+{
+    public class Patient
+    {
+        // Id, UserId (FK), DateOfBirth, Gender, BloodType, Address, User (nav), Appointments (nav)
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; } = default!;
+        public string BloodType { get; set; } = default!;
+
+        public string Address { get; set; } = default!;
+        // Navigation properties
+        //public User User { get; set; } = default!;
+        public List<Appointment> Appointments { get; set; } = [];
+
+
+
+    }
+}
