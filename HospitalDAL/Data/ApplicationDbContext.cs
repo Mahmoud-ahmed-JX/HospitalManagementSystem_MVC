614ebe3d3c3bbc4637b450bd3196d788e571d3ff
Mahmoud-ahmed-JX <shahdelfares515@gmail.com>
2025-12-22 15:04:15 +0200

    Added Enums 3 classes and dbContext

---
+++ HospitalDAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDAL.Data
+{
+
+    internal class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional configuration can go here
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Entities.ApplicationUser> ApplicationUsers { get; set; } = null!;
+        public DbSet<Entities.Doctor> Doctors { get; set; } = null!;
+        public DbSet<Entities.Department> Departments { get; set; } = null!;
+
+    }
+}
---
+++ HospitalDAL/Enums/AppointmentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDAL.Enums
+{
+    internal enum AppointmentStatus
+    {
+        Pending=1,
+        Confirmed,
+        Completed,
+        Cancelled
+    }
+}
